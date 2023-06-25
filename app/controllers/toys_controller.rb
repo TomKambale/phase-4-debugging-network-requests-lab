7caa263b284611a8d43141fbe7e72c2eb61027d3
Thomas <thomaskambale1@gmail.com>
2023-06-25 17:52:48 +0300

    completed assignment, revisit

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,14 +7,30 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy
+      if toy.update(toy_params)
+        render json: toy
+      else
+        render json: { error: toy.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Toy not found" }, status: :not_found
+    end
   end
+    # def update
+  #   toy = Toy.find_by(id: params[:id])
+  #   if toy.update(toy_params)
+  #   render json: toy
+  #   else 
+  #     render json: { error: toy.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
@@ -25,7 +41,7 @@ class ToysController < ApplicationController
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:name, :image, :likes, :id)
   end
 
 end
